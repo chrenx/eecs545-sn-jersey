abc81a3de8ac4d44d10e8b3e26378f5c28ed83e3
chrenx <chrenx@umich.edu>
2024-03-09 17:04:54 -0500

    update

---
+++ data/soccernet_dataset.py
@@ -0,0 +1,15 @@
+from torch.utils.data import Dataset
+
+
+class SoccerNet(Dataset):
+    def __init__(self):
+        super().__init__()
+        
+
+    def __getitem__(self, idx):
+       
+        return records     
+    
+    def __len__(self):
+        return None
+    
