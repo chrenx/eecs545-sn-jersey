d2fdd43285c1bb38f85e87a19fe442922151b156
chrenx <chrenx@umich.edu>
2024-03-26 12:01:44 -0400

    update

--- predict_soccernet.py
+++ predict_soccernet.py
@@ -1,4 +1,4 @@
-import os, json, glob
+import os, json, glob, torch, heapq
 from ultralytics import YOLO
 from argparse import ArgumentParser
 
@@ -29,9 +29,8 @@ def process_data(model, args):
             continue
 
         #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        img_folder_name = 1
+        img_folder_name = "1"
         img_dir = os.path.join(img_folder_path, img_folder_name)
-        
         
         num_store = [0]*101
         box_center_store = [None]*100
@@ -46,12 +45,13 @@ def process_data(model, args):
                          for img_name in all_img_names if img_name[0] != '.']
         
         #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        results = model.predict(all_img_paths[-100:], save=False, conf=0.60)
+        results = model.predict(all_img_paths[-100:], save=False, 
+                                conf=args.conf, verbose=False)
         
-        # print('当下tracklet参与预测的图片数量: ', len(results))
+        print('当下tracklet参与预测的图片数量: ', len(results))
 
         # process results of all images in one tracklet 
-        for res in results:
+        for idx, res in enumerate(results):
             # no digits detected
             if len(res.obb.cls) == 0:
                 jn = 100
@@ -62,10 +62,19 @@ def process_data(model, args):
             # two digits detected
             else:
                 two_digit_detected = True
+                print('predicted: ', res.obb.cls)
+                print('path: ', all_img_paths[-100+idx])
                 first_digit = int(res.obb.cls[0].item())
                 second_digit = int(res.obb.cls[1].item())
                 jn = first_digit * 10 + second_digit
             
+            if len(res.obb.cls) != 0:
+                x = torch.Tensor([p[0] for p in res.obb.xyxyxyxyn[0,:]])
+                y = torch.Tensor([p[1] for p in res.obb.xyxyxyxyn[0,:]])
+                centroid = ((torch.sum(x) / 4).item(), 
+                            (torch.sum(y) / 4).item())
+                box_center_store[jn] = centroid
+                            
             num_store[jn] += 1
 
             # finish early if possible
@@ -76,21 +85,35 @@ def process_data(model, args):
                 found = True
                 final_res = jn
                 predict_dict[img_folder_name] = final_res
+                print("这里1")
                 break
 
         if found:
             # process next folder
+            print("这里2")
             break
         else:
             if two_digit_detected:
+                print("这里3")
                 # didn't pass the threshold, but it is ok for now
-                final_res = num_store.index(max(num_store[10:100]))
+                print('max: ', max(num_store[10:100]))
+                final_res = num_store[10:100].index(max(num_store[10:100])) + 10  
+                print("final res: ", final_res)
                 predict_dict[img_folder_name] = final_res
                 # process next folder
                 break
             
         # some strategies to process the digits detected
-        if 
+        # two digits: but img catches only one for each
+        single_digit = num_store[:10]
+        first_digit, second_digit = heapq.nlargest(2, range(len(single_digit)), 
+                                                   key=single_digit.__getitem__)
+        first_centroid = box_center_store[first_digit]
+        second_centroid = box_center_store[second_digit]
+        if first_centroid[0] > second_centroid[0]:
+            final_res = first_digit * 10 + second_digit
+        else:
+            final_res = second_digit * 10 + first_digit
 
 
         predict_dict[img_folder_name] = final_res
