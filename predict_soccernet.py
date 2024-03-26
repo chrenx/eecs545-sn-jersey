ad60fdc9ed74696a28f80687f8396deb969bed2e
chrenx <chrenx@umich.edu>
2024-03-26 10:59:36 -0400

    update

--- predict_soccernet.py
+++ predict_soccernet.py
@@ -5,7 +5,8 @@ from argparse import ArgumentParser
 
 def process_data(model, args):
     input_path = args.input_path
-    output_dir = args.output_dir
+    output_pred_dir = f"predict_{args.mode}.json"
+    incorrect_pred_dir = f'predict_{args.mode}_incorrect.json'
 
     img_folder_path = os.path.join(input_path, 'images')
     img_folder_names = os.listdir(img_folder_path)
@@ -19,6 +20,7 @@ def process_data(model, args):
             gt_dict = json.load(f)
 
     predict_dict = {}
+    incorrect_dict = {}
 
     # iterate the images folder
     for img_folder_name in img_folder_names[:4]:
@@ -26,9 +28,11 @@ def process_data(model, args):
         if img_folder_name[0] == '.':
             continue
 
+        #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        img_folder_name = 1
         img_dir = os.path.join(img_folder_path, img_folder_name)
         
-        # process all images in one tracklet
+        
         num_store = [0]*101
         box_center_store = [None]*100
         final_res = None
@@ -40,16 +44,22 @@ def process_data(model, args):
        
         all_img_paths = [os.path.join(img_dir, img_name) \
                          for img_name in all_img_names if img_name[0] != '.']
-        results = model.predict(all_img_paths[:100], save=False, conf=0.60)
         
-        print('当下tracklet参与预测的图片数量: ', len(results))
+        #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        results = model.predict(all_img_paths[-100:], save=False, conf=0.60)
+        
+        # print('当下tracklet参与预测的图片数量: ', len(results))
 
+        # process results of all images in one tracklet 
         for res in results:
+            # no digits detected
             if len(res.obb.cls) == 0:
                 jn = 100
+            # one digit detected
             elif len(res.obb.cls) == 1:
                 jn = int(res[0].obb.cls[0].item())
                 
+            # two digits detected
             else:
                 two_digit_detected = True
                 first_digit = int(res.obb.cls[0].item())
@@ -58,28 +68,35 @@ def process_data(model, args):
             
             num_store[jn] += 1
 
-            # finish early if could
-            if jn != 100 and num_store[jn] >= args.threshold and two_digit_detected:
-                # just in case most imgs detect single digit while there is
+            # finish early if possible
+            if jn != 100 and num_store[jn] >= args.threshold and \
+               two_digit_detected and jn >= 10:
+                # just in case most imgs detect single digit when there is
                 # only a few has two digits
                 found = True
                 final_res = jn
+                predict_dict[img_folder_name] = final_res
                 break
 
-        if not found:
+        if found:
+            # process next folder
+            break
+        else:
             if two_digit_detected:
+                # didn't pass the threshold, but it is ok for now
                 final_res = num_store.index(max(num_store[10:100]))
+                predict_dict[img_folder_name] = final_res
+                # process next folder
+                break
             
+        # some strategies to process the digits detected
+        if 
 
-        if num_store[final_res] < 1:
-            final_res = -1
 
-        # write result to file
         predict_dict[img_folder_name] = final_res
         print(predict_dict)
 
-    with open(output_dir, 'w') as f:
-        json.dump(predict_dict, f)
+        return ##
 
     # calculate accuracy
     if not args.challenge:
@@ -88,18 +105,29 @@ def process_data(model, args):
         for key, val in predict_dict.items():
             if val == gt_dict[key]:
                 correct += 1
+            else:
+                incorrect_dict[key] = val
         accuracy = correct / count * 100
         print(f"accuracy for {args.mode}: {accuracy}%")
+
+        with open(incorrect_pred_dir, 'w') as f:
+            json.dump(incorrect_dict, f)
+
+    # write result to file
+    with open(output_pred_dir, 'w') as f:
+        json.dump(predict_dict, f)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
+    parser.add_argument("--conf", type=float, default=0.6)
+    parser.add_argument("--mode", type=str, default='train')
     parser.add_argument("--challenge", action="store_true")
-    parser.add_argument("--threshold", type=int, default=70)
-    parser.add_argument("--output_dir", type=str, default='./predict.json')
+    parser.add_argument("--threshold", type=int, default=5)
     parser.add_argument("--input_path", type=str, default='data/jersey-2023/train')
     parser.add_argument("--model_path", type=str, default='yolo-bb/best-03-25.pt')
-    args = parser.parse_args()
+    # args = parser.parse_args()
+    args, unknown = parser.parse_known_args()
 
     model = YOLO(args.model_path)
 
